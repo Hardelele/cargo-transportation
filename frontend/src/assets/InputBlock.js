00cfbe9066f0eacf4b5d18526a0bbb663f202692
Hardelele <hardelele@yahoo.com>
2019-10-19 19:14:28 +0300

    Added InputBlock class - import InputBlock.css - Added <div> className="InputBlock" - Added action form - export default InputBlock;

---
+++ frontend/src/assets/InputBlock.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './InputBlock.css';
+
+function InputBlock() {
+    return (
+        <div className="InputBlock">
+            <form action={"/"}>
+                <input placeholder={"login"} className={"input"} name={"login"} />
+                <br/>
+                <input placeholder={"password"} className={"input"} name={"password"}/>
+                <br/>
+                <input type={"submit"} name={"send"} className={"button"} value={"Sign in"}/>
+            </form>
+        </div>
+    );
+}
+
+export default InputBlock;

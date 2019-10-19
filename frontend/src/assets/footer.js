f7e7eff936e1ffd94f01d7e6e99c613bc324a82a
Hardelele <hardelele@yahoo.com>
2019-10-19 19:11:09 +0300

    Add footer.js - import footer.css - configure export - Add <div> class="footer"

---
+++ frontend/src/assets/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+let footer = () => {
+    return (
+        <div className={"footer"}>
+            Developer: Vladislav Lapshin Social:
+        </div>
+    );
+}
+
+export default footer;

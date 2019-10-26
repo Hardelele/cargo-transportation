9f2f0aba1c9f0b7a5f9d424d2c91063c98bbb041
Hardelele <hardelele@yahoo.com>
2019-10-26 10:21:58 +0300

    Edit C.T.Application Added script that filled table by two trips

--- backend/src/main/java/com/hardelele/ct/CargoTransportationApplication.java
+++ backend/src/main/java/com/hardelele/ct/CargoTransportationApplication.java
@@ -1,13 +1,29 @@
 package com.hardelele.ct;
 
+import com.hardelele.ct.services.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CargoTransportationApplication {
 
+    private final TripService tripService;
+
+    @Autowired
+    public CargoTransportationApplication(TripService tripService) {
+        this.tripService = tripService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CargoTransportationApplication.class, args);
+    }
+
+    @Bean
+    public void fillTripsTable() {
+        tripService.add("Санкт-Петербург","Москва",4,2,5000,"21 08 2019", "25 08 2019");
+        tripService.add("Москва","Рига",3,5,9000,"18 08 2019", "26 08 2019");
     }
 
 }

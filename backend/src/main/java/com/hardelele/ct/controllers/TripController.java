9b3273d5d51360204c03ef252d42f768907f3588
Hardelele <hardelele@yahoo.com>
2019-10-26 10:20:06 +0300

    Edit TripController added new mapping Add map for /trips/{id}, that take single trip by id

--- backend/src/main/java/com/hardelele/ct/controllers/TripController.java
+++ backend/src/main/java/com/hardelele/ct/controllers/TripController.java
@@ -2,8 +2,10 @@ package com.hardelele.ct.controllers;
 
 import com.hardelele.ct.models.TripEntity;
 import com.hardelele.ct.services.TripService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,5 +23,10 @@ public class TripController {
     @GetMapping(value = "/trips")
     public List<TripEntity> getAll() {
         return tripService.getAll();
+    }
+
+    @GetMapping(value = "/trips/{id}")
+    public TripEntity getOne(@PathVariable Long id) throws NotFoundException {
+        return tripService.getOneById(id);
     }
 }

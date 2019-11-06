b5d7360ae5c1791b8534d1441384d37cb75a6590
Hardelele <hardelele@yahoo.com>
2019-11-06 19:01:07 +0200

    Add auto entity from snapshot-r.0-v.1

---
+++ backend/src/main/java/com/hardelele/ct/models/AutoEntity.java
@@ -0,0 +1,63 @@
+package com.hardelele.ct.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "auto")
+public class AutoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "mark")
+    private String mark;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "number")
+    private long number;
+
+    public AutoEntity() {
+        
+    }
+
+    public AutoEntity(String mark, String model, long number) {
+        this.mark = mark;
+        this.model = model;
+        this.number = number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public void setNumber(long number) {
+        this.number = number;
+    }
+}

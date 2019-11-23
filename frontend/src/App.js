bf1bb23c6bbc50abc2308c37cf06aa82edb9839b
Hardelele <i.lapshina@mail.ru>
2019-11-23 07:28:56 +0300

    App: add first layout on page - Add MainContainer, Container, HeaderButton and other classes - Add 3 buttons on page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,18 @@
 import React, {Component} from 'react';
-import axios from "axios";
 import './css/App.css';
 
 class App extends Component {
 
-    state = {
-        trips: []
-    };
-
-    componentDidMount() {
-        axios.get('https://localhost:8081/trips')
-            .then(response => {
-                console.log(this.state.trips);
-                const trips = response.data;
-                this.setState({
-                    trips
-                });
-            })
-    }
-
     render() {
         return (
-            <div className="App">
-
-                { this.state.trips.map(trip => <div>{trip.destination}</div>)}
+            <div className="MainContainer">
+                <header className="Header Container">
+                    <div className="HeaderButton">Панель</div>
+                    <div className="HeaderButton">Калькулятор</div>
+                    <div className="HeaderButton">Документы</div>
+                </header>
+                <div className="Content Container"></div>
+                <footer className="Footer Container"></footer>
             </div>
         );
     }

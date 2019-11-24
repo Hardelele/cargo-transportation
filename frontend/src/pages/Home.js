74093c9bb52802514d1d264af4ff1075ee031461
Hardelele <i.lapshina@mail.ru>
2019-11-24 16:12:01 +0300

    Home: Add exports, imports and move code from App.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+
+export const Home = () => {
+    return (
+        <Fragment>
+            <div className="content__container home__container">
+                <div className="home__content">
+
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+

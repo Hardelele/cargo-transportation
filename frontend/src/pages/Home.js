502d4a598d3653af985e2f6ba70e82597ca1c52b
Hardelele <i.lapshina@mail.ru>
2019-11-29 22:23:25 +0300

    Add Home.js page and design it - add div structure for flex layout - make top and center containers - make login_button "SIGN IN"

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,19 +1,44 @@
-import React, {Fragment} from 'react';
-import '../css/pages/home.css';
-import '../css/components/article.css';
+import React, {Component} from 'react';
+import './css/home.css';
 
-export const Home = () => {
-    return (
-        <Fragment>
-            <div className="content__container home__container">
-                <div className="home__content">
-                    <div className="article">
-                        <div className='article_title'>О приложении</div>
-                        <div className="article_content"></div>
+class Home extends Component {
+    render() {
+        return (
+            <div className="home__container">
+                <div className="top__container top__background">
+                    <div className="login_button">SIGN IN</div>
+                    <div className="empty_space"></div>
+                    <div className="top_logo__container">
+                        <div className="logo_margin"></div>
+                        Cargo <br/> Transportation
+                    </div>
+                </div>
+                <div className="center__container">
+                    <div className="center__empty_space"></div>
+                    <div className="center_logo__container">About company</div>
+                    <div className="center__empty_space"></div>
+                    <div className="center_content">
+                        <div className="card">
+                            <div className="title_card">Activity of the company</div>
+                            <div className="text_card"></div>
+                        </div>
+                        <div className="card">
+                            <div className="title_card">Tariffs</div>
+                            <div className="text_card"></div>
+                        </div>
+                        <div className="card">
+                            <div className="title_card">Vacancies</div>
+                            <div className="text_card"></div>
+                        </div>
+                        <div className="card">
+                            <div className="title_card">Contact us</div>
+                            <div className="text_card"></div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </Fragment>
-    )
+        );
+    }
 }
 
+export default Home;

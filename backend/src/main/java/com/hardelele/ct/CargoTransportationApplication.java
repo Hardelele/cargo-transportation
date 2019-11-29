5bef30ff60c61726502b0a21d9a325e413a8ac32
Hardelele <i.lapshina@mail.ru>
2019-11-29 22:29:55 +0300

    CargoTransportationApplication: fixed bug - Fixed bug on frontend: Added Bean WebMvcConfigurer: corsConfigurer

--- backend/src/main/java/com/hardelele/ct/CargoTransportationApplication.java
+++ backend/src/main/java/com/hardelele/ct/CargoTransportationApplication.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CargoTransportationApplication {
@@ -24,6 +27,16 @@ public class CargoTransportationApplication {
     public void fillTripsTable() {
         tripService.add("Санкт-Петербург","Москва",4,2,5000,"21 08 2019", "25 08 2019");
         tripService.add("Москва","Рига",3,5,9000,"18 08 2019", "26 08 2019");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }

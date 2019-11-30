9c7be058983012f5bc29410e987ffe316657cab6
Hardelele <i.lapshina@mail.ru>
2019-11-30 23:10:53 +0300

    panel.js: realize header container

--- frontend/src/pages/Panel.js
+++ frontend/src/pages/Panel.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
+import "./css/panel.css"
 
 class Panel extends Component {
     render() {
         return (
             <div className="panel__container">
                 <div className="header-panel">
-
+                    <div className="header_logo">
+                        <div className="title-upper">Cargo Transportation</div>
+                        <div className="title-bottom">Control panel</div>
+                    </div>
+                    <nav className="header_nav__container">
+                        <div className="header_button"></div>
+                    </nav>
+                    <div className="profile__container">
+                        <div className="profile_img"></div>
+                    </div>
                 </div>
                 <div className="center-panel"></div>
                 <div className="footer-panel"></div>

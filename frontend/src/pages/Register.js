362ace2e8a4c72cdf8316fa9bdc89e2472bde0d5
Hardelele <i.lapshina@mail.ru>
2019-11-30 22:04:19 +0300

    Add empty Register.js component and configure it

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Register extends Component {
+    render() {
+        return (
+            <div className="reg__container">
+            </div>
+        );
+    }
+}
+
+export default Register;

2c5c0265cd83431c36f3b177475295acd3906fd8
Hardelele <i.lapshina@mail.ru>
2019-12-01 16:02:52 +0300

    PanelNav.js: add nav buttons Management, Documents and Dashboard

--- frontend/src/pages/components/PanelNav.js
+++ frontend/src/pages/components/PanelNav.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import {NavLink} from "react-router-dom";
-import './css/login_btn.css';
+import './css/panel_nav.css';
 
 class PanelNav extends Component {
     render() {
         return (
             <nav className="header_nav__container">
-                <div className="button__container">
-                    <NavLink className="header_button" to="panel/manage">Database</NavLink>
-                </div>
+                <NavLink className="header_button" to="manage">Management</NavLink>
+                <NavLink className="header_button" to="docs">Documents</NavLink>
+                <NavLink className="header_button" to="dash">Dashboard</NavLink>
             </nav>
         );
     }

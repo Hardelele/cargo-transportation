db90b1959908d77dfb94a73bb5ea82c63e26a899
Hardelele <i.lapshina@mail.ru>
2019-12-04 02:48:19 +0300

    Add UserRepository and extend JpaRepository

---
+++ backend/src/main/java/com/hardelele/ct/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.hardelele.ct.repositories;
+
+import com.hardelele.ct.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+}

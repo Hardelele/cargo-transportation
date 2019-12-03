c64dff9a23ec0d7d7532a96eeef7ce4165b36068
Hardelele <i.lapshina@mail.ru>
2019-12-04 02:50:22 +0300

    TripService: fix bugs with update mapping   - Change type from void to TripEntity

--- backend/src/main/java/com/hardelele/ct/services/TripService.java
+++ backend/src/main/java/com/hardelele/ct/services/TripService.java
@@ -10,7 +10,7 @@ public interface TripService {
     List<TripEntity> getAll();
     Optional<TripEntity> getOneById(Long id);
     TripEntity add(String departure, String destination, int driverId, int carId, double driverPrice, String startDate, String endDate);
-    void update(long id, String departure, String destination, int driverId, int carId, double driverPrice, String startDate, String endDate);
+    TripEntity update(long id, String departure, String destination, int driverId, int carId, double driverPrice, String startDate, String endDate);
     void delete(long id);
     void deleteAll();
 }

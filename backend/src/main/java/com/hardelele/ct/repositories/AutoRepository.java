6822e17187723040d0b50cf23a5ed788d64ce8fb
Hardelele <i.lapshina@mail.ru>
2019-12-23 07:39:31 +0300

    Add empty AutoRepository

--- backend/src/main/java/com/hardelele/ct/repositories/AutoRepository.java
+++ backend/src/main/java/com/hardelele/ct/repositories/AutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AutoRepository extends JpaRepository<AutoModel,Long> {
+public interface AutoRepository extends JpaRepository<AutoModel,Integer> {
 }

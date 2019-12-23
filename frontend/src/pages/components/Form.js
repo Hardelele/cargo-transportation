58560967599d075348913ee6f78c6ec9fbcfd1e7
Hardelele <i.lapshina@mail.ru>
2019-12-23 07:33:19 +0300

    Added form container form that getting trips from ct api and print it

--- frontend/src/pages/components/Form.js
+++ frontend/src/pages/components/Form.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import './css/form.css';
 
 class Form extends Component {
 
@@ -8,7 +9,7 @@ class Form extends Component {
     }
 
     componentDidMount () {
-        axios.get(`http://localhost:8080/trips`).then(res => {
+        axios(`http://localhost:8080/trips`).then(res => {
                 const trips = res.data;
                 console.log(res.data);
                 this.setState({ trips });
@@ -17,9 +18,30 @@ class Form extends Component {
 
     render() {
         return (
-            <ul>
-                { this.state.trips.map(trip => <li key={trip.id}>{trip.departure} -> {trip.destination}</li>)}
-            </ul>
+            <div className="form__container">
+                <div className="form_row form_top">
+                    <div className="form_short_elem">id</div>
+                    <div className="form_mid_elem">departure</div>
+                    <div className="form_mid_elem">destination</div>
+                    <div className="form_elem">driver id</div>
+                    <div className="form_elem">car id</div>
+                    <div className="form_mid_elem">driver price</div>
+                    <div className="form_mid_elem">start date</div>
+                    <div className="form_mid_elem">end date</div>
+                </div>
+                { this.state.trips.map(trip =>
+                    <div className="form_row">
+                        <div className="form_short_elem">{trip.id}</div>
+                        <div className="form_mid_elem">{trip.departure}</div>
+                        <div className="form_mid_elem">{trip.destination}</div>
+                        <div className="form_elem">{trip.driverId}</div>
+                        <div className="form_elem">{trip.carId}</div>
+                        <div className="form_mid_elem">{trip.driverPrice}</div>
+                        <div className="form_mid_elem">{trip.startDate}</div>
+                        <div className="form_mid_elem">{trip.endDate}</div>
+                    </div>
+                )}
+            </div>
         );
     }
 }
